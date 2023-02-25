e27ed362ff9439220138230c505fd10af8fa4c3e
Sniper53 <33094221+Sniper53@users.noreply.github.com>
2023-02-25 18:14:26 +0300

    Add files via upload

---
+++ 09/UDPsocket/udpworker.h
@@ -0,0 +1,31 @@
+#ifndef UDPWORKER_H
+#define UDPWORKER_H
+
+#include <QUdpSocket>
+#include <QNetworkDatagram>
+
+
+#define BIND_PORT 12345
+
+class UDPworker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UDPworker(QObject *parent = nullptr);
+    void InitSocket( void );
+    void ReadDatagram( QNetworkDatagram datagram);
+    void SendDatagram(QByteArray data );
+
+
+private slots:
+    void readPendingDatagrams(void);
+
+private:
+    QUdpSocket* serviceUdpSocket;
+
+signals:
+    void sig_sendDataToGUI(QByteArray data);
+
+};
+
+#endif // UDPWORKER_H
