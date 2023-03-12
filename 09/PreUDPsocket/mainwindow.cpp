b3d653cff1f108ab03c12b46f263ed98dad50de1
Sniper53 <33094221+Sniper53@users.noreply.github.com>
2023-03-12 11:25:57 +0300

    Add files via upload

---
+++ 09/PreUDPsocket/mainwindow.cpp
@@ -0,0 +1,62 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    udpWorker = new UDPworker(this);
+    udpWorker->InitSocket();
+
+    connect(udpWorker, &UDPworker::sig_sendDataToGUI, this, &MainWindow::PorcessReceiveData);
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, [&]{
+
+        QDateTime dateTime = QDateTime::currentDateTime();
+
+        QByteArray dataToSend;
+        QDataStream outStr(&dataToSend, QIODevice::WriteOnly);
+
+        outStr << dateTime;
+
+        udpWorker->SendDatagram(dataToSend);
+        timer->start(TIMER_DELAY);
+
+    });
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pb_start_clicked()
+{
+    timer->start(TIMER_DELAY);
+}
+
+
+void MainWindow::PorcessReceiveData(QDateTime data)
+{
+    counterPck++;
+    if(counterPck % 20 == 0){
+        ui->te_result->clear();
+    }
+
+    ui->te_result->append("Текущее время: " + data.toString() + ". "
+                "Принято пакетов " + QString::number(counterPck));
+
+
+}
+
+
+void MainWindow::on_pb_stop_clicked()
+{
+    timer->stop();
+}
+
---
+++ 09/PreUDPsocket/udpworker.cpp
@@ -0,0 +1,68 @@
+#include "udpworker.h"
+
+UDPworker::UDPworker(QObject *parent) : QObject(parent)
+{
+
+
+
+}
+
+
+/*!
+ * @brief Метод инициализирует UDP сервер
+ */
+void UDPworker::InitSocket()
+{
+
+    serviceUdpSocket = new QUdpSocket(this);
+    /*
+     * Соединяем присваиваем адрес и порт серверу и соединяем функцию
+     * обраотчик принятых пакетов с сокетом
+     */
+    serviceUdpSocket->bind(QHostAddress::LocalHost, BIND_PORT);
+
+    connect(serviceUdpSocket, &QUdpSocket::readyRead, this, &UDPworker::readPendingDatagrams);
+
+}
+
+/*!
+ * @brief Метод осуществляет обработку принятой датаграммы
+ */
+void UDPworker::ReadDatagram(QNetworkDatagram datagram)
+{
+
+    QByteArray data;
+    data = datagram.data();
+
+
+    QDataStream inStr(&data, QIODevice::ReadOnly);
+    QDateTime dateTime;
+    inStr >> dateTime;
+
+    emit sig_sendDataToGUI(dateTime);
+}
+/*!
+ * @brief Метод осуществляет опередачу датаграммы
+ */
+void UDPworker::SendDatagram(QByteArray data)
+{
+    /*
+     *  Отправляем данные на localhost и задефайненный порт
+     */
+    serviceUdpSocket->writeDatagram(data, QHostAddress::LocalHost, BIND_PORT);
+}
+
+/*!
+ * @brief Метод осуществляет чтение датаграм из сокета
+ */
+void UDPworker::readPendingDatagrams( void )
+{
+    /*
+     *  Производим чтение принятых датаграмм
+     */
+    while(serviceUdpSocket->hasPendingDatagrams()){
+            QNetworkDatagram datagram = serviceUdpSocket->receiveDatagram();
+            ReadDatagram(datagram);
+    }
+
+}
